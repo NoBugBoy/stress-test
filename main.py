dbb81fd9f0d040dd21643adb86ceb5a24c40fa13
yujian <754369677@qq.com>
2020-07-14 18:46:41 +0800

    test

--- main.py
+++ main.py
@@ -26,32 +26,32 @@ if __name__ == '__main__':
     # http://nuhx0mrq.xiaomy.net/gateway/trade/api/art/preBuy
     # http://nuhx0mrq.xiaomy.net/gateway/trade/api/art/buy
 
-    thread_count = 10
+    thread_count = 2000
     method = 'POST'
     param = '{}'
-    # request_url = 'http://101.200.212.244/gateway/trade/api/artTopic/list/3'
+    request_url = 'http://101.200.212.244/gateway/trade/api/art/preBuy'
     # # 多少秒执行完成 0代表并发
     slowTime = 0
-    url = ''
+    # url = ''
     # # 循环执行次数
     roundCount = 1
     # # 是否从文件读取数据
-    read = False
+    read = True
 
-    if len(sys.argv) > 3:
-        thread_count = int(sys.argv[1])
-        method = str(sys.argv[2])
-        request_url = str(sys.argv[3])
-        if len(sys.argv) > 4:
-            url = str(sys.argv[4])
-    else:
-        print('参数数量错误')
-        exit(1)
+    # if len(sys.argv) > 3:
+    #     thread_count = int(sys.argv[1])
+    #     method = str(sys.argv[2])
+    #     request_url = str(sys.argv[3])
+    #     if len(sys.argv) > 4:
+    #         url = str(sys.argv[4])
+    # else:
+    #     print('参数数量错误')
+    #     exit(1)
     # load数据
 
     startTime = time.time()
-    if len(url) > 0:
-        data.loadRequestData(url)
-        log.logger.info("读取数据消耗时间{}毫秒".format((time.time() - startTime) * 1000))
-        check_param(thread_count - 1, request_url, method, param)
+
+    data.loadRequestData('/Users/yujian/Desktop/StressTestData-202007130427.txt')
+    log.logger.info("读取数据消耗时间{}毫秒".format((time.time() - startTime) * 1000))
+    check_param(thread_count - 1, request_url, method, param)
     sc.start(slowTime, roundCount, thread_count, request_url, method, param, read)
